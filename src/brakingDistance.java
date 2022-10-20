bd937365a4b5a140f30bdfd84f40c7ba64d4f76d
levaered <vova21sergeev@gmail.com>
2022-10-20 10:29:14 +0300

    breakingDiistanceForDryRoad

--- src/brakingDistance.java
+++ src/brakingDistance.java
@@ -3,7 +3,38 @@ import java.util.Scanner;
 public class brakingDistance {
     Scanner in = new Scanner(System.in);
     int brakingFactor = 1;
-    public void brakingDistance(int carSpeed, int roarCondotion, int typeOfRoadSurface){
-
+    double s;
+    public void brakingDistance(int carSpeed, int roadCondotion, int typeOfRoadSurface){
+        if(roadCondotion == 1){
+            switch (typeOfRoadSurface){
+                case 1:
+                    s = (brakingFactor * (carSpeed * carSpeed)) / (254 * 0.8);
+                    break;
+                case 2:
+                    s = (brakingFactor * (carSpeed * carSpeed)) / (254 * 0.7);
+                    break;
+                case 3:
+                    s = (brakingFactor * (carSpeed * carSpeed)) / (254 * 0.7);
+                    break;
+                case 4:
+                    s = (brakingFactor * (carSpeed * carSpeed)) / (254 * 0.6);
+                    break;
+                case 5:
+                    s = (brakingFactor * (carSpeed * carSpeed)) / (254 * 0.55);
+                    break;
+            }
+        }
+        if(roadCondotion == 2){
+            switch (typeOfRoadSurface){
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+            }
+        }
     }
 }
