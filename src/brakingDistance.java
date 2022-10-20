e724d5bd2253d84e20c0fb7bebef6c4118287588
levaered <vova21sergeev@gmail.com>
2022-10-20 10:09:32 +0300

    new updates

--- src/brakingDistance.java
+++ src/brakingDistance.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class brakingDistance {
+import java.util.Scanner;
+
+public class brakingDistance {
+    Scanner in = new Scanner(System.in);
+    int brakingFactor = 1;
+    public void brakingDistance(int carSpeed, int roarCondotion, int typeOfRoadSurface){
+
+    }
 }
